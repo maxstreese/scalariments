96b3153caeba3da91f84447665b13d5f83ec02e9
Max Streese <max.streese@smarttra.de>
2021-07-26 13:52:14 +0200

    Make akka-debezium use Kafka as the offset store

--- akka-debezium/src/main/scala/com/streese/akka/debezium/Main.scala
+++ akka-debezium/src/main/scala/com/streese/akka/debezium/Main.scala
@@ -1,6 +1,7 @@
 package com.streese.akka.debezium
 
 import com.typesafe.scalalogging.StrictLogging
+import io.debezium.embedded.Connect
 import io.debezium.engine.DebeziumEngine
 import io.debezium.engine.format.Avro
 import io.debezium.engine.format.Json
@@ -20,16 +21,20 @@ object Main extends App with StrictLogging {
 
   val props = {
     val c = Map(
-      "name"                         -> "debezium_test",
-      "connector.class"              -> "io.debezium.connector.postgresql.PostgresConnector",
-      "offset.storage.file.filename" -> "/tmp/debezium_test_offsets.dat",
-      "database.hostname"            -> "localhost",
-      "database.port"                -> "5432",
-      "database.user"                -> "admin",
-      "database.password"            -> "password",
-      "database.dbname"              -> "postgres",
-      "database.server.name"         -> "debezium_test",
-      "plugin.name"                  -> "pgoutput"
+      "name"                              -> "debezium_test",
+      "bootstrap.servers"                 -> "localhost:9092",
+      "connector.class"                   -> "io.debezium.connector.postgresql.PostgresConnector",
+      "plugin.name"                       -> "pgoutput",
+      "offset.storage"                    -> "org.apache.kafka.connect.storage.KafkaOffsetBackingStore",
+      "offset.storage.topic"              -> "debezium_test_offset",
+      "offset.storage.partitions"         -> "1",
+      "offset.storage.replication.factor" -> "1",
+      "database.hostname"                 -> "localhost",
+      "database.port"                     -> "5432",
+      "database.user"                     -> "admin",
+      "database.password"                 -> "password",
+      "database.dbname"                   -> "postgres",
+      "database.server.name"              -> "debezium_test"
     ).asJava
     val p = new Properties()
     p.putAll(c)
@@ -37,7 +42,7 @@ object Main extends App with StrictLogging {
   }
 
   val engine = DebeziumEngine
-    .create(classOf[Json])
+    .create(classOf[Connect])
     .using(props)
     .notifying(r => logger.info(s"Key: ${r.key}\tValue: ${r.value}"))
     .build()
