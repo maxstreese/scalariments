52184452d2dea00a317189a086eb0af8e12dab99
Max Streese <max.streese@smarttra.de>
2021-08-17 11:21:55 +0200

    Add another step

---
+++ microservices-with-akka/1-shopping-cart-grpc/shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
@@ -0,0 +1,17 @@
+package shopping.cart
+
+import scala.concurrent.Future
+
+import org.slf4j.LoggerFactory
+
+class ShoppingCartServiceImpl extends proto.ShoppingCartService {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  override def addItem(in: proto.AddItemRequest): Future[proto.Cart] = { 
+    logger.info("addItem {} to cart {}", in.itemId, in.cartId)
+    Future.successful(
+      proto.Cart(items = List(proto.Item(in.itemId, in.quantity))))
+  }
+
+}
