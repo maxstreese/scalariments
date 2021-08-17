ed742d9fae95b43598e8027a76a2e6bc33708557
Max Streese <max.streese@smarttra.de>
2021-08-17 13:10:11 +0200

    Add more stuff

---
+++ microservices-with-akka/2-shopping-cart-event-sourced/shopping-order-service/src/main/scala/shopping/order/Main.scala
@@ -0,0 +1,30 @@
+package shopping.order
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorSystem
+import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.management.scaladsl.AkkaManagement
+import org.slf4j.LoggerFactory
+import scala.util.control.NonFatal
+
+object Main {
+
+  val logger = LoggerFactory.getLogger("shopping.order.Main")
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem[Nothing](Behaviors.empty, "ShoppingOrderService")
+    try {
+      init(system)
+    } catch {
+      case NonFatal(e) =>
+        logger.error("Terminating due to initialization failure.", e)
+        system.terminate()
+    }
+  }
+
+  def init(system: ActorSystem[_]): Unit = {
+    AkkaManagement(system).start()
+    ClusterBootstrap(system).start()
+  }
+
+}
